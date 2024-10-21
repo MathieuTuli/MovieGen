343048098308a6ce27978012de157d0878f67745
Mathieu Tuli <tuli.mathieu@gmail.com>
2024-10-21 19:06:07 -0400

    Update

--- tests/test_tae.py
+++ tests/test_tae.py
@@ -16,7 +16,6 @@ def test_tae_encoder():
     tae_config = TAEConfig()
     tae = TAE(tae_config)
     tae.to(device)
-    tae.from_pretrained("pretrained-weights/tae/model.ckpt")
     x = torch.zeros((3, 1, 3, 32, 32), device=device)  # [b, seq, c, w, h]
     x = tae.encode(x)
     B, T, C, W, H = x.sample().shape
@@ -46,7 +45,6 @@ def test_tae_decoder():
     tae_config = TAEConfig()
     tae = TAE(tae_config)
     tae.to(device)
-    tae.from_pretrained("pretrained-weights/tae/model.ckpt")
     x = torch.zeros((3, 1, 16, 4, 4), device=device)  # [b, seq, c, w, h]
     tae.decode(x)
     x = torch.zeros((3, 3, 16, 3, 3), device=device)  # [b, seq, c, w, h]
@@ -62,7 +60,6 @@ def test_tae_forward():
     tae_config = TAEConfig()
     tae = TAE(tae_config)
     tae.to(device)
-    tae.from_pretrained("pretrained-weights/tae/model.ckpt")
     x = torch.zeros((3, 1, 3, 32, 32), device=device)  # [b, seq, c, w, h]
     dec, post = tae(x)
     assert dec.shape == x.shape
